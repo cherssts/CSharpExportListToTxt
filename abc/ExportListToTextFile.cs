866c78489e2b609b9ebed03ccd6391586200dd8c
Chersstss <alexanderfan02@gmail.com>
2022-07-23 19:23:49 -0500

    First Commit

---
+++ abc/ExportListToTextFile.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace abc
+{
+    public static class ExportListToTextFile
+    {
+
+        static void Main(string[] args)
+        {
+            var data = new Person[] {
+            new Person() { ID = 1, FirstName = "John", Surname = "Smith" },
+            new Person() { ID = 2, FirstName = "Max", Surname = "McDonald" }
+            };
+            data.ExportToTextFile("person.txt", ';');
+        }
+
+        static void ExportToTextFile<T>(this IEnumerable<T> data, string FileName, char ColumnSeperator)
+        {
+            using (var sw = File.CreateText(FileName))
+            {
+                var plist = typeof(T).GetProperties().Where(p => p.CanRead && (p.PropertyType.IsValueType || p.PropertyType == typeof(string)) && p.GetIndexParameters().Length == 0).ToList();
+                if (plist.Count > 0)
+                {
+                    var seperator = ColumnSeperator.ToString();
+                    sw.WriteLine(string.Join(seperator, plist.Select(p => p.Name)));
+                    foreach (var item in data)
+                    {
+                        var values = new List<object>();
+                        foreach (var p in plist) values.Add(p.GetValue(item, null));
+                        sw.WriteLine(string.Join(seperator, values));
+                    }
+                }
+            }
+        }
+
+    }
+    class Person
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string Surname { get; set; }
+    }
+}
---
+++ abc/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Linq;
+using System.IO;
+using System.Threading;
+using System.Data;
+
+
+
+namespace Program1111
+{
+    public class Program1
+    {
+        static void Main(string[] args)
+        {
+            
+            string _arrString = "Write Your Stuff";
+            char Sperator = ';';
+            //Console.WriteLine(_arrString.Count());
+            string[] Separated = _arrString.Split(Sperator);
+
+            //string authors = "Mahesh Chand, Henry He, Chris Love, Raj Beniwal, Praveen Kumar";
+            //string stringBeforeChar = authors.Substring(0, authors.IndexOf(","));
+            //Console.WriteLine(stringBeforeChar);
+
+            //string stringAfterChar = authors.Substring(authors.IndexOf(",") + 2);
+            //Console.WriteLine(stringAfterChar);
+            List<string> _stringList = new List<string>();
+            for (int i = 0; i < Separated.Length; i++)
+            {
+                switch (i)
+                {
+                    case 0:
+                        Console.WriteLine("Server= " + Separated[i] + ';');
+                        _stringList.Add("Server= " + Separated[i] + ';');
+                        break;
+                    case 1:
+                        Console.WriteLine("Port= " + Separated[i] + ';');
+                        _stringList.Add("Port = " + Separated[i] + ';');
+                        break;
+                    case 2:
+                        Console.WriteLine("UserID= " + Separated[i] + ';');
+                        _stringList.Add("UserID= " + Separated[i] + ';');
+                        break;
+                    case 3:
+                        Console.WriteLine("Password= " + Separated[i] + ';');
+                        _stringList.Add("Password= " + Separated[i] + ';');
+                        break;
+                    case 4:
+                        Console.WriteLine("Database = " + Separated[i] + ';');
+                        _stringList.Add("Database = " + Separated[i] + ';');
+                        break;
+                    default:
+                        break;
+                }
+            }
+
+            foreach (var item in _stringList)
+            {
+                Console.WriteLine(item.Substring(item.IndexOf("=") + 2));
+            }
+            Console.ReadLine();
+        }
+    }
+}
